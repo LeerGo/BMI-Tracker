accf737239618f82751927f5df6b0a742ab82b5c
Li Yi Fang <a94118@126.com>
2020-12-08 17:22:09 +0800

    init repo

---
+++ app/src/main/java/fun/bugfix/bmi/MainActivity.kt
@@ -0,0 +1,17 @@
+package `fun`.bugfix.bmi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 1. 建立个人档案 年龄、性别、身高、体重
+ * 2. 输入目标体重
+ * 4. 输入当前体重
+ * 5. 设置功能 - 语言、提醒、数据备份（先不做）
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
